88322e4da87bb188743b83c21ff6b013a95d5d5e
Darell Mona <s2200064@student.unklab.ac.id>
2023-10-04 10:47:51 +0800

    feat: slicing BA -> dospem -> StudentProfile

--- src/app/routes/dosenPembimbingRoutes.js
+++ src/app/routes/dosenPembimbingRoutes.js
@@ -1,10 +1,15 @@
 import Page from "@jumbo/shared/Page";
 import StudentInformation from "app/pages/BimbinganAkademik/DesenPembimbing/StudentInformation";
+import StudentProfile from "app/pages/BimbinganAkademik/DesenPembimbing/StudentInformation/StudentProfile";
 
 const dosenPembimbingRoutes = [
   {
     path: "/bimbingan-akademik/student-information",
     element: <Page component={StudentInformation} />,
+  },
+  {
+    path: "/bimbingan-akademik/student-information/:id",
+    element: <Page component={StudentProfile} />,
   },
 ];
 
