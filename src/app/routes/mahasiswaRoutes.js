bf3219b1639dd978cb1f780c7c588138d36dce29
Frances Rully Yong <frances.yong080900@gmail.com>
2023-10-04 20:36:00 +0800

    fix: router mahasiswa and dosen. page dosen.  menu dosen uri

--- src/app/routes/mahasiswaRoutes.js
+++ src/app/routes/mahasiswaRoutes.js
@@ -1,21 +1,40 @@
 import Page from "@jumbo/shared/Page";
-import DaftarAlumni from "app/pages/Mahasiswa/KlabatBridge/DaftarAlumni";
-import BerandaMahasiswa from "app/pages/ThesisApps/Mahasiswa/BerandaMahasiswa";
+import DaftarPengajuan from "app/pages/ThesisApps/Mahasiswa/DaftarPengajuan";
 import BerandaPengajuanJudul from "app/pages/ThesisApps/Mahasiswa/BerandaPengajuanJudul";
-import DaftarMahasiswa from "app/pages/ThesisApps/Mahasiswa/DaftarMahasiswa";
-import Konsultasi from "app/pages/ThesisApps/Mahasiswa/Konsultasi";
 import PengajuanJudul from "app/pages/ThesisApps/Mahasiswa/PengajuanJudul";
 import PengajuanJudulDiterima from "app/pages/ThesisApps/Mahasiswa/PengajuanJudulDiterima";
+import BerandaPengajuanSkripsi from "app/pages/ThesisApps/Mahasiswa/BerandaPengajuanSkripsi";
+import Konsultasi from "app/pages/ThesisApps/Mahasiswa/Konsultasi";
 import UploadProposal from "app/pages/ThesisApps/Mahasiswa/UploadProposal";
+// ++ UploadRevisi
 
 const mahasiswaRoutes = [
   {
-    path: "/klabat-bridge/daftar-alumni",
-    element: <Page component={DaftarAlumni} />,
-    // Thesis Apps Mahasiswa
     path: "/sistem-informasi-skripsi/daftar-pengajuan",
-    element: <Page component={DaftarMahasiswa} />,
+    element: <Page component={DaftarPengajuan} />,
   },
+  {
+    path: "/sistem-informasi-skripsi/daftar-pengajuan/beranda-pengajuan-judul",
+    element: <Page component={BerandaPengajuanJudul} />,
+  },
+  {
+    path: "/sistem-informasi-skripsi/daftar-pengajuan/beranda-pengajuan-skripsi",
+    element: <Page component={BerandaPengajuanSkripsi} />,
+  },
+  // beranda pengajuan proposal dan skripsi = sama (hanya ganti isisan)
+  {
+    path: "/sistem-informasi-skripsi/daftar-pengajuan/konsultasi-mahasiswa",
+    element: <Page component={Konsultasi} />,
+  },
+  {
+    path: "/sistem-informasi-skripsi/daftar-pengajuan/pengajuan-judul",
+    element: <Page component={PengajuanJudul} />,
+  },
+  {
+    path: "/sistem-informasi-skripsi/daftar-pengajuan/pengajuan-judul-diterima",
+    element: <Page component={PengajuanJudulDiterima} />,
+  },
+
 ];
 
 export default mahasiswaRoutes;
