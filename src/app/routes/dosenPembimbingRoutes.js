3050a08f58039bdcf6754556ce331d53c0556b45
Darell Mona <s2200064@student.unklab.ac.id>
2023-10-04 15:35:48 +0800

    feat: BA -> dospem -> StudentGrade

--- src/app/routes/dosenPembimbingRoutes.js
+++ src/app/routes/dosenPembimbingRoutes.js
@@ -1,5 +1,7 @@
+import StudentGradeDashboard from "app/pages/BimbinganAkademik/DosenPembimbing/StudentInformation/StudentGradeDashboard";
 import StudentInformation from "../pages/BimbinganAkademik/DosenPembimbing/StudentInformation";
 import StudentProfile from "../pages/BimbinganAkademik/DosenPembimbing/StudentInformation/StudentProfile";
+import StudentGrade from "app/pages/BimbinganAkademik/DosenPembimbing/StudentInformation/StudentGradeDashboard/StudentGrade";
 
 const { default: Page } = require("@jumbo/shared/Page");
 
@@ -11,6 +13,14 @@ const dosenPembimbingRoutes = [
   {
     path: "/bimbingan-akademik/student-information/:id",
     element: <Page component={StudentProfile} />,
+  },
+  {
+    path: "/bimbingan-akademik/student-information/:id/grade",
+    element: <Page component={StudentGradeDashboard} />,
+  },
+  {
+    path: "/bimbingan-akademik/student-information/:id/grade/semester/:number",
+    element: <Page component={StudentGrade} />,
   },
 ];
 
