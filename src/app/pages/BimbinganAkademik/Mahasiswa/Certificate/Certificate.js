47c66897f83e6f102cc6242cba6e3f2fd97c56f6
Hendra Luntungan <hendraluntungan29@gmail.com>
2023-10-04 14:20:30 +0800

    Fix: Bimbingan Akademik -> Certificate

--- src/app/pages/BimbinganAkademik/Mahasiswa/Certificate/Certificate.js
+++ src/app/pages/BimbinganAkademik/Mahasiswa/Certificate/Certificate.js
@@ -1,126 +1,381 @@
-import Div from "@jumbo/shared/Div";
+import React, { useState, useEffect } from "react";
 import {
-  Button,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
   Typography,
-  Pagination,
+  TextField,
+  Stack,
+  Grid,
+  FormControl,
+  Box,
+  Button,
+  Input,
+  IconButton,
 } from "@mui/material";
-import ActionButton from "app/shared/ActionButton";
-import SearchGlobal from "app/shared/SearchGlobal";
-import React, { useState } from "react";
-import CreateIcon from "@mui/icons-material/Create";
-import MarkunreadIcon from "@mui/icons-material/Markunread";
-import SearchLocal from "./SearchLocal";
-import AddIcon from "@mui/icons-material/Add";
+import MenuItem from "@mui/material/MenuItem";
 import { Link } from "react-router-dom";
+import SaveAltIcon from "@mui/icons-material/SaveAlt";
+import Modal from "@mui/material/Modal";
+import CloseIcon from "@mui/icons-material/Close";
+
+const requiredStyle = {
+  color: "red",
+  marginLeft: "4px",
+};
+
+function RTypography({ children, sx }) {
+  return (
+    <Typography variant="body1" sx={sx}>
+      {children}
+      <span style={requiredStyle}>*</span>
+    </Typography>
+  );
+}
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  boxShadow: 24,
+  padding: 24,
+  backgroundColor: "white",
+  borderRadius: 10,
+  maxWidth: "90%",
+  "@media (max-width: 768px)": {
+    maxWidth: "80%",
+  },
+  "@media (max-width: 480px)": {
+    maxWidth: "80%",
+  },
+};
+
+const style2 = {
+  position: "fixed",
+  top: "15%",
+  right: "2%",
+  width: 400,
+  // bgcolor: "background.paper",
+  boxShadow: 24,
+  padding: 24,
+  backgroundColor: "white",
+  borderRadius: 10,
+};
 
 const Certificate = () => {
-  const [page, setPage] = useState(1);
+  const [category, setCategory] = useState("");
+  const [showLabel, setShowLabel] = useState(true);
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [selectedFileName, setSelectedFileName] = useState("");
 
-  const handleChange = (e, p) => {
-    setPage(p);
+  const [openFirstModal, setOpenFirstModal] = React.useState(false);
+  const [openSecondModal, setOpenSecondModal] = React.useState(false);
+
+  const handleOpenFirstModal = () => setOpenFirstModal(true);
+  const handleCloseFirstModal = () => setOpenFirstModal(false);
+  const handleOpenSecondModal = () => setOpenSecondModal(true);
+  const handleCloseSecondModal = () => setOpenSecondModal(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      handleCloseSecondModal();
+    }, 5000); // 5000 ms = 5 detik
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [handleOpenSecondModal]);
+
+  const handleFileInputChange = (event) => {
+    const file = event.target.files[0];
+    setSelectedFile(file);
+
+    const labelElement = document.getElementById("certificate-label");
+    if (labelElement) {
+      labelElement.style.border = "0.5px solid #BCBCBC";
+    }
+
+    if (file) {
+      setSelectedFileName(file.name);
+    } else {
+      setSelectedFileName("");
+    }
   };
 
-  const TableItem = ({ index }) => (
-    <TableRow
-      style={{ textDecoration: "none" }}
-      component={Link}
-      to="/bimbingan-akademik/certificates/student-certificate-waiting"
-    >
-      <TableCell>{index + 1}</TableCell>
-      <TableCell>10 May 2000</TableCell>
-      <TableCell>Menang lomba desain prototype</TableCell>
-      <TableCell>Faculty</TableCell>
-      <TableCell>Sertifikat menang lomba.pdf</TableCell>
-      <TableCell>Approved</TableCell>
-    </TableRow>
-  );
+  const handleSubmitFirstModal = () => {
+    handleCloseFirstModal();
+    handleOpenSecondModal();
+  };
 
   return (
-    <Div>
-      <Div
+    <div>
+      <Typography
         sx={{
-          display: "flex",
-          direction: "row",
-          justifyContent: "space-between",
-          alignItems: "center",
+          fontSize: "24px",
+          fontWeight: 500,
           paddingBottom: "24px",
         }}
       >
-        <Typography sx={{ fontSize: "24px", fontWeight: 500, gap: "24px" }}>
-          All Certifications
-        </Typography>
-        <Div
-          sx={{
-            display: "flex",
-            direction: "row",
-            gap: 3,
-            alignItems: "center",
-          }}
-        >
-          <SearchLocal />
-          <Link
-            style={{ textDecoration: "none", color: "white" }}
-            to="/bimbingan-akademik/certificates/add-new"
-          >
-            <Button
-              sx={{
-                backgroundColor: "#006AF5",
-                borderRadius: "24px",
-                color: "white",
-                whiteSpace: "nowrap",
-                minWidth: "132px",
-                fontSize: "12px",
-                padding: "10px",
-                display: "flex",
-                alignItems: "center",
-                gap: "6px",
+        Add New Certificate
+      </Typography>
 
-                "&:hover": {
-                  backgroundColor: "#025ED8",
-                },
+      <Stack spacing={2} sx={{ paddingBottom: 3 }}>
+        <RTypography>Title</RTypography>
+        <TextField
+          id="outlined-basic-1"
+          variant="outlined"
+          placeholder="Ex. Menang lomba desain prototype"
+          fullWidth
+          sx={{ height: "48px" }}
+        />
+      </Stack>
+
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={6}>
+          <Stack spacing={2} sx={{ paddingBottom: 3 }}>
+            <RTypography sx={{ paddingBottom: "10px" }}>Category</RTypography>
+            <TextField
+              sx={{ width: "100%" }}
+              id="outlined-select-category"
+              select
+              label={showLabel ? "Select Certificate Category" : ""}
+              value={category}
+              onChange={(event) => {
+                setCategory(event.target.value);
+                setShowLabel(false);
+              }}
+              InputLabelProps={{
+                shrink: false,
               }}
             >
-              {" "}
-              <AddIcon sx={{ fontSize: "medium" }} />
-              Add New
-            </Button>
+              <MenuItem value="local">Local</MenuItem>
+              <MenuItem value="national">National</MenuItem>
+              <MenuItem value="international">International</MenuItem>
+            </TextField>
+          </Stack>
+        </Grid>
+
+        <Grid item xs={12} md={6}>
+          <Stack spacing={2} sx={{ paddingBottom: 3 }}>
+            <RTypography sx={{ paddingBottom: "10px" }}>
+              Certificate Photo
+            </RTypography>
+            <FormControl fullWidth variant="outlined">
+              <Input
+                type="file"
+                accept=".jpg, .jpeg, .png"
+                id="certificate-photo"
+                onChange={handleFileInputChange}
+                disableUnderline
+                inputProps={{ style: { display: "none" } }}
+              />
+              <label
+                htmlFor="certificate-photo"
+                style={{
+                  border: "0.5px solid #BCBCBC",
+                  padding: "14px",
+                  height: "53px",
+                  borderRadius: "4px",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                  transition: "border-color 0.3s ease",
+                  cursor: "pointer",
+                }}
+                onMouseEnter={(e) => {
+                  e.currentTarget.style.borderColor = "black";
+                }}
+                onMouseLeave={(e) => {
+                  e.currentTarget.style.borderColor = "#BCBCBC";
+                }}
+                onClick={(e) => {
+                  e.currentTarget.style.border = "2px solid #006AF5";
+                }}
+                id="certificate-label"
+              >
+                <span style={{ color: "#9E9E9E" }}>
+                  {selectedFileName || "Import Photo (Maximum Size 2MB)"}
+                </span>
+                <SaveAltIcon style={{ color: "#9E9E9E" }} />
+              </label>
+            </FormControl>
+          </Stack>
+        </Grid>
+
+        <Grid item xs={12}>
+          <Stack spacing={2} sx={{ paddingBottom: 3 }}>
+            <Typography>Descriptions</Typography>
+            <TextField
+              id="outlined-basic"
+              variant="outlined"
+              placeholder="Add Descriptions"
+              fullWidth
+              multiline
+            />
+          </Stack>
+        </Grid>
+
+        <Grid item xs={12}>
+          <Link
+            style={{ textDecoration: "none", color: "white" }}
+            to="/bimbingan-akademik/certificates/"
+          >
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "flex-end",
+                marginTop: "20px",
+              }}
+            >
+              <Link
+                style={{ textDecoration: "none", color: "white" }}
+                to="/bimbingan-akademik/certificates/"
+              >
+                <Button
+                  sx={{
+                    backgroundColor: "darkgrey",
+                    borderRadius: "24px",
+                    color: "black",
+                    whiteSpace: "nowrap",
+                    minWidth: "132px",
+                    fontSize: "12px",
+                    padding: "10px",
+                    marginRight: "24px",
+
+                    "&:hover": {
+                      backgroundColor: "grey",
+                    },
+                  }}
+                >
+                  Cancel
+                </Button>
+              </Link>
+
+              <Button
+                onClick={handleOpenFirstModal}
+                sx={{
+                  backgroundColor: "#006AF5",
+                  borderRadius: "24px",
+                  color: "white",
+                  whiteSpace: "nowrap",
+                  minWidth: "132px",
+                  fontSize: "12px",
+                  padding: "10px",
+                  gap: "6px",
+
+                  "&:hover": {
+                    backgroundColor: "#025ED8",
+                  },
+                }}
+              >
+                Submit
+              </Button>
+              <Modal
+                open={openFirstModal}
+                onClose={handleCloseFirstModal}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+              >
+                <div style={style}>
+                  <Typography
+                    id="modal-modal-title"
+                    variant="h4"
+                    component="h2"
+                    sx={{
+                      fontWeight: 600,
+                    }}
+                  >
+                    Send Certificate?
+                  </Typography>
+                  <Typography
+                    id="modal-modal-description"
+                    style={{ marginTop: "16px", marginBottom: "20px" }}
+                  >
+                    Are you sure you want to submit this? Forms that have been
+                    submitted cannot be edited again.
+                  </Typography>
+
+                  <Grid container spacing={1} justifyContent="flex-end">
+                    <Grid item>
+                      <Button
+                        onClick={handleCloseFirstModal}
+                        sx={{
+                          backgroundColor: "white",
+                          borderRadius: "5px",
+                          boxShadow: 4,
+                          color: "black",
+                          whiteSpace: "nowrap",
+                          "&:hover": {
+                            backgroundColor: "lightgrey",
+                          },
+                        }}
+                      >
+                        Cancel
+                      </Button>
+                    </Grid>
+                    <Grid item>
+                      <Button
+                        onClick={handleSubmitFirstModal}
+                        sx={{
+                          backgroundColor: "#006AF5",
+                          borderRadius: "5px",
+                          boxShadow: 4,
+                          color: "white",
+                          whiteSpace: "nowrap",
+                          "&:hover": {
+                            backgroundColor: "#025ED8",
+                          },
+                        }}
+                      >
+                        Submit
+                      </Button>
+                    </Grid>
+                  </Grid>
+                </div>
+              </Modal>
+              <Modal
+                open={openSecondModal}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+              >
+                <div style={style2}>
+                  <IconButton
+                    edge="end"
+                    color="#D9D9D9"
+                    onClick={handleCloseSecondModal}
+                    aria-label="close"
+                    sx={{
+                      position: "absolute",
+                      top: "10px",
+                      right: "20px",
+                    }}
+                  >
+                    <CloseIcon />
+                  </IconButton>
+                  <Typography
+                    id="modal-modal-title"
+                    variant="h4"
+                    component="h2"
+                    sx={{
+                      fontWeight: 600,
+                    }}
+                  >
+                    Successful Submission!
+                  </Typography>
+                  <Typography
+                    id="modal-modal-description"
+                    style={{ marginTop: "16px", marginBottom: "20px" }}
+                  >
+                    You have successfully preregistered for the course.
+                  </Typography>
+                  {/* Tambahkan tautan dan elemen lain yang diperlukan di sini */}
+                </div>
+              </Modal>
+            </Box>
           </Link>
-        </Div>
-      </Div>
-      <TableContainer
-        sx={{
-          overflow: "auto",
-          marginBottom: "30px",
-        }}
-      >
-        <Table>
-          <TableHead sx={{ backgroundColor: "rgba(26, 56, 96, 0.1)" }}>
-            <TableRow>
-              <TableCell>Number</TableCell>
-              <TableCell>Submission Date</TableCell>
-              <TableCell>Title</TableCell>
-              <TableCell>Category</TableCell>
-              <TableCell>Certificate</TableCell>
-              <TableCell>Status</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {[...Array(10)].map((item, index) => (
-              <TableItem index={index} />
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <div>
-        <Pagination count={10} color="primary" onChange={handleChange} />
-      </div>
-    </Div>
+        </Grid>
+      </Grid>
+    </div>
   );
 };
 
