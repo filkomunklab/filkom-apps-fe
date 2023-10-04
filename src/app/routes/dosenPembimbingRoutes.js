10b0f3bdb7c9c174b2e84000fa282ec34271e204
sherrenkalalo <s2200043@student.unklab.ac.id>
2023-10-04 15:18:14 +0800

    Feat: Bimbingan Akademik -> Dospem -> History; Mahasiswa -> History (modified)

--- src/app/routes/dosenPembimbingRoutes.js
+++ src/app/routes/dosenPembimbingRoutes.js
@@ -1,8 +1,7 @@
-import StudentInformation from "../pages/BimbinganAkademik/DosenPembimbing/StudentInformation";
-import StudentProfile from "../pages/BimbinganAkademik/DosenPembimbing/StudentInformation/StudentProfile";
+import Page from "@jumbo/shared/Page";
+import StudentInformation from "app/pages/BimbinganAkademik/DesenPembimbing/StudentInformation";
+import StudentProfile from "app/pages/BimbinganAkademik/DesenPembimbing/StudentInformation/StudentProfile";
 import History from "app/pages/BimbinganAkademik/DesenPembimbing/History";
-
-const { default: Page } = require("@jumbo/shared/Page");
 
 const dosenPembimbingRoutes = [
   {
