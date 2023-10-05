8eff993fa646890be0f61e12d833d2b73047c56b
Darell Mona <s2200064@student.unklab.ac.id>
2023-10-05 14:27:15 +0800

    feat: slicing BA -> dospem -> Student Certificate, SearchLocal component

---
+++ src/app/pages/BimbinganAkademik/DosenPembimbing/StudentInformation/StudentCertificate/StudentCertificate.js
@@ -0,0 +1,147 @@
+import Div from "@jumbo/shared/Div";
+import {
+  FormControl,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Select,
+  Stack,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TablePagination,
+  TableRow,
+  Typography,
+} from "@mui/material";
+import SearchLocal from "app/shared/SearchLocal";
+import { useState } from "react";
+
+const data = [...Array(15)].map(() => ({
+  submissionDate: "10 May 2000",
+  title: "Menang Lomba Desain Prototype",
+  category: "Faculty",
+  certifacePhoto: "Sertifikat menang lomba.pdf",
+  description:
+    "Saya mengikuti lomba desain prototype website kampus yang diselenggarakan oleh Fakultas Ilmu Komputer",
+  status: "Approved",
+}));
+
+const StudentCertificate = () => {
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [sortBy, setSortBy] = useState("");
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+  return (
+    <Div>
+      <Stack gap={3}>
+        <Stack direction={"row"} justifyContent={"space-between"}>
+          <Typography variant="h1">All Certifications</Typography>
+          <Typography variant="h6">Yuhu, Darell Deil</Typography>
+        </Stack>
+        <Grid container spacing={2} alignItems={"center"}>
+          <Grid item md={6}>
+            <Typography variant="h6">
+              Here is the data of the attached certificates belonging to this
+              student.
+            </Typography>
+          </Grid>
+          <Grid item md={3}>
+            <SearchLocal />
+          </Grid>
+          <Grid item md={3}>
+            <FormControl sx={{ width: "100%" }}>
+              <InputLabel id="sort-label">Sort By</InputLabel>
+              <Select
+                sx={{ borderRadius: "24px" }}
+                labelId="sort-component"
+                id="demo-simple-select-helper"
+                value={sortBy}
+                label="Sort By"
+                onChange={(event) => setSortBy(event.target.value)}
+              >
+                <MenuItem value="">
+                  <em>None</em>
+                </MenuItem>
+                <MenuItem value={"Empty"}>Empty</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+        </Grid>
+        <Table sx={{ overflowX: "auto" }}>
+          <TableHead>
+            <TableHeading />
+          </TableHead>
+          <TableBody>
+            {data
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((item, index) => (
+                <TableItem index={index} key={index} item={item} />
+              ))}
+          </TableBody>
+        </Table>
+        <TablePagination
+          rowsPerPageOptions={[10, 25]}
+          component={"div"}
+          count={data.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Stack>
+    </Div>
+  );
+};
+
+const TableHeading = () => {
+  const style = { fontWeight: 400, whiteSpace: "nowrap" };
+  return (
+    <TableRow sx={{ backgroundColor: "#1A38601A" }}>
+      <TableCell sx={[style]}>No</TableCell>
+      <TableCell sx={[style]}>Submission Date</TableCell>
+      <TableCell sx={[style]}>Title</TableCell>
+      <TableCell sx={[style]}>Category</TableCell>
+      <TableCell sx={[style]}>Certificate Photo</TableCell>
+      <TableCell sx={[style]}>Description</TableCell>
+      <TableCell sx={[style]}>Status</TableCell>
+    </TableRow>
+  );
+};
+
+const TableItem = ({ item, index }) => {
+  let statusColor;
+
+  switch (item.status) {
+    case "Waiting":
+      statusColor = "#FFCC00";
+      break;
+    case "Approved":
+      statusColor = "#005FDB";
+      break;
+    case "Rejected":
+      statusColor = "#E21D12";
+      break;
+  }
+  return (
+    <TableRow>
+      <TableCell>{index + 1}</TableCell>
+      <TableCell>{item.submissionDate}</TableCell>
+      <TableCell>{item.title}</TableCell>
+      <TableCell>{item.category}</TableCell>
+      <TableCell>{item.certifacePhoto}</TableCell>
+      <TableCell>{item.description}</TableCell>
+      <TableCell sx={{ color: statusColor }}>{item.status}</TableCell>
+    </TableRow>
+  );
+};
+
+export default StudentCertificate;
--- src/app/routes/dosenPembimbingRoutes.js
+++ src/app/routes/dosenPembimbingRoutes.js
@@ -2,7 +2,10 @@ import StudentGradeDashboard from "app/pages/BimbinganAkademik/DosenPembimbing/S
 import StudentInformation from "../pages/BimbinganAkademik/DosenPembimbing/StudentInformation";
 import StudentProfile from "../pages/BimbinganAkademik/DosenPembimbing/StudentInformation/StudentProfile";
 import StudentGrade from "app/pages/BimbinganAkademik/DosenPembimbing/StudentInformation/StudentGradeDashboard/StudentGrade";
+import StudentCertificate from "app/pages/BimbinganAkademik/DosenPembimbing/StudentInformation/StudentCertificate";
 import History from "app/pages/BimbinganAkademik/DesenPembimbing/History";
+
+const { default: Page } = require("@jumbo/shared/Page");
 
 const dosenPembimbingRoutes = [
   {
@@ -20,6 +23,10 @@ const dosenPembimbingRoutes = [
   {
     path: "/bimbingan-akademik/student-information/:id/grade/semester/:number",
     element: <Page component={StudentGrade} />,
+  },
+  {
+    path: "/bimbingan-akademik/student-information/:id/certificate",
+    element: <Page component={StudentCertificate} />,
   },
   {
     path: "/bimbingan-akademik/dospem/history",
