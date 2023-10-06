8c0d0799af0ae24bdddec522d30b5944a427b08f
sherrenkalalo <s2200043@student.unklab.ac.id>
2023-10-06 10:34:31 +0800

    feat: Bimbingan Akademik -> Mahasiswa -> History (Modified)

--- src/app/pages/BimbinganAkademik/Mahasiswa/History/History.js
+++ src/app/pages/BimbinganAkademik/Mahasiswa/History/History.js
@@ -455,7 +455,7 @@ const History = (props) => {
               <ListItem
                 button
                 component={Link}
-                to="/bimbingan-akademik/grades/studentgrade/"
+                to="/bimbingan-akademik/history/student-certificate-waiting"
                 sx={{ paddingLeft: "50px", paddingRight: "50px" }}
               >
                 <ListItemText
@@ -509,7 +509,7 @@ const History = (props) => {
               <ListItem
                 button
                 component={Link}
-                to="/bimbingan-akademik/grades/studentgrade/"
+                to="/bimbingan-akademik/history/student-certificate-approved"
                 sx={{ paddingLeft: "50px", paddingRight: "50px" }}
               >
                 <ListItemText
@@ -549,7 +549,7 @@ const History = (props) => {
               <ListItem
                 button
                 component={Link}
-                to="/bimbingan-akademik/grades/studentgrade/"
+                to="/bimbingan-akademik/history/student-certificate-rejected"
                 sx={{ paddingLeft: "50px", paddingRight: "50px" }}
               >
                 <ListItemText
@@ -589,7 +589,7 @@ const History = (props) => {
               <ListItem
                 button
                 component={Link}
-                to="/bimbingan-akademik/grades/studentgrade/"
+                to="/bimbingan-akademik/history/student-certificate-approved"
                 sx={{ paddingLeft: "50px", paddingRight: "50px" }}
               >
                 <ListItemText
@@ -667,7 +667,7 @@ const History = (props) => {
               <ListItem
                 button
                 component={Link}
-                to="/bimbingan-akademik/grades/studentgrade/"
+                to="/bimbingan-akademik/history/grade-waiting"
                 sx={{ paddingLeft: "50px", paddingRight: "50px" }}
               >
                 <ListItemText
@@ -722,7 +722,7 @@ const History = (props) => {
               <ListItem
                 button
                 component={Link}
-                to="/bimbingan-akademik/grades/studentgrade/"
+                to="/bimbingan-akademik/history/grade-approved"
                 sx={{ paddingLeft: "50px", paddingRight: "50px" }}
               >
                 <ListItemText
