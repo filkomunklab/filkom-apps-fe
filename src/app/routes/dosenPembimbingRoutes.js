2bd35fa31e42609dea883aaa8e5587e8e444f31c
Darell Mona <s2200064@student.unklab.ac.id>
2023-10-09 11:52:22 +0800

    feat: slicing BA -> dospem -> dashboard

--- src/app/routes/dosenPembimbingRoutes.js
+++ src/app/routes/dosenPembimbingRoutes.js
@@ -1,3 +1,4 @@
+import Dashboard from "../pages/BimbinganAkademik/DosenPembimbing/Dashboard";
 import StudentGradeDashboard from "app/pages/BimbinganAkademik/DosenPembimbing/StudentInformation/StudentGradeDashboard";
 import StudentInformation from "../pages/BimbinganAkademik/DosenPembimbing/StudentInformation";
 import StudentProfile from "../pages/BimbinganAkademik/DosenPembimbing/StudentInformation/StudentProfile";
@@ -9,6 +10,10 @@ import CertificateDetail from "app/pages/BimbinganAkademik/DosenPembimbing/Stude
 const { default: Page } = require("@jumbo/shared/Page");
 
 const dosenPembimbingRoutes = [
+  {
+    path: "/bimbingan-akademik/dashboard",
+    element: <Page component={Dashboard} />,
+  },
   {
     path: "/bimbingan-akademik/student-information",
     element: <Page component={StudentInformation} />,
