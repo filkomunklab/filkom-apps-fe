5eba9d56ae2af5baa4f455a6d8d7e67a058f85a5
OvelTjia05 <oveltjia05@gmail.com>
2023-10-15 00:36:37 +0800

    feat: slicing BA -> Dekan -> Review Activities -> Review Pre-regis

--- src/app/routes/dekanRoutes.js
+++ src/app/routes/dekanRoutes.js
@@ -7,6 +7,8 @@ import StudentGrade from "app/pages/BimbinganAkademik/Dekan/StudentInformation/S
 import StudentCertificate from "app/pages/BimbinganAkademik/Dekan/StudentInformation/StudentCertificate";
 import StudentGradeDashboard from "app/pages/BimbinganAkademik/Dekan/StudentInformation/StudentGradeDashboard";
 import CertificateDetail from "app/pages/BimbinganAkademik/Dekan/StudentInformation/StudentCertificate/CertificateDetail";
+import ReviewPreRegistration from "app/pages/BimbinganAkademik/Dekan/ReviewActivities/ReviewPreRegistration";
+import ReviewPreRegistrationStudent from "app/pages/BimbinganAkademik/Dekan/ReviewActivities/ReviewPreRegistration/ReviewPreRegistrationStudent";
 //import SupervisorInformation from "app/pages/BimbinganAkademik/Dekan/SupervisorInformation";
 
 const { default: Page } = require("@jumbo/shared/Page");
@@ -47,6 +49,14 @@ const dekanRoutes = [
   {
     path: "/bimbingan-akademik/dekan/student-information/:id/certificate/:id",
     element: <Page component={CertificateDetail} />,
+  },
+  {
+    path: "/bimbingan-akademik/dekan/review-activities/pre-registration",
+    element: <Page component={ReviewPreRegistration} />,
+  },
+  {
+    path: "/bimbingan-akademik/dekan/review-activities/pre-registration/:id",
+    element: <Page component={ReviewPreRegistrationStudent} />,
   },
   // {
   //   path: "/bimbingan-akademik/dekan/supervisor-information",
