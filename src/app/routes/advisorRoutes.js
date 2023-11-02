e5bc40740985df33cf10782651b0bcfd95011e07
geovalga <liemgeovalga@gmail.com>
2023-11-02 23:55:45 +0800

    memperbaiki roll dan menambahkan roll advisor

--- src/app/routes/advisorRoutes.js
+++ src/app/routes/advisorRoutes.js
@@ -3,21 +3,21 @@ import BerandaProposalPenguji from "app/pages/ThesisApps/Dosen/BerandaProposalPe
 import BuatKonsultasiAdvisor from "app/pages/ThesisApps/Dosen/BuatKonsultasiAdvisor";
 import DaftarBimbinganProposalAdvisor from "app/pages/ThesisApps/Dosen/DaftarBimbinganProposalAdvisor";
 import DaftarBimbinganSkripsiAdvisor from "app/pages/ThesisApps/Dosen/DaftarBimbinganSkripsiAdvisor";
+import DocumentPersetujuanDosenPembimbing from "app/pages/ThesisApps/Dosen/DocumentPersetujuanDosenPembimbing";
 import DocumentRevisiProposalAdvisor from "app/pages/ThesisApps/Dosen/DocumentRevisiProposalAdvisor";
 import MengisiBeritaAcaraProposalKetuaPenelis from "app/pages/ThesisApps/Dosen/MengisiBeritaAcaraProposalKetuaPenelis";
 import RiwayatBimbinganAdvisor from "app/pages/ThesisApps/Dosen/RiwayatBimbinganAdvisor";
-import BerandaProposalMahasiswa from "app/pages/ThesisApps/Mahasiswa/BerandaProposalMahasiswa";
 
 const advisorRoutes = [
   {
     // Daftar Bimbingan Proposal Advisor
-    path: "/sistem-informasi-skripsi/bimbingan-proposal-advisor",
+    path: "/sistem-informasi-skripsi/daftar-bimbingan-proposal-advisor",
     element: <Page component={DaftarBimbinganProposalAdvisor} />,
   },
 
   {
     // Daftar Bimbingan Skripsi Advisor
-    path: "/sistem-informasi-skripsi/bimbingan-skripsi-advisor",
+    path: "/sistem-informasi-skripsi/daftar-bimbingan-skripsi-advisor",
     element: <Page component={DaftarBimbinganSkripsiAdvisor} />,
   },
 
@@ -28,34 +28,44 @@ const advisorRoutes = [
   },
   {
     // View Beranda Mahasiswa proposal
-    path: "/sistem-informasi-skripsi/bimbingan-proposal-advisor/beranda",
+    path: "/sistem-informasi-skripsi/daftar-bimbingan-proposal-advisor/beranda",
     element: <Page component={BerandaProposalPenguji} />,
   },
   {
     // Catat Konsultasi Mahasiswa
-    path: "/sistem-informasi-skripsi/bimbingan-proposal-advisor/konsultasi",
+    path: "/sistem-informasi-skripsi/daftar-bimbingan-proposal-advisor/konsultasi",
     element: <Page component={BuatKonsultasiAdvisor} />,
   },
   {
     // Dokumen Proposal
-    // path: "/sistem-informasi-skripsi/bimbingan-proposal-advisor/dokumen-proposal",
-    // element: <Page component={} />,
-  },
-  {
-    // Dokumen Proposal
-    // path: "/sistem-informasi-skripsi/bimbingan-proposal-advisor/dokumen-proposal",
-    // element: <Page component={} />,
+    path: "/sistem-informasi-skripsi/daftar-bimbingan-proposal-advisor/dokumen-proposal",
+    element: <Page component={DocumentPersetujuanDosenPembimbing} />,
   },
   {
     // Berita Acara proposal
-    path: "/sistem-informasi-skripsi/bimbingan-proposal-advisor/berita-acara-proposal",
+    path: "/sistem-informasi-skripsi/daftar-bimbingan-proposal-advisor/berita-acara-proposal",
     element: <Page component={MengisiBeritaAcaraProposalKetuaPenelis} />,
   },
   {
     // Dokumen revisi proposal
-    path: "/sistem-informasi-skripsi/bimbingan-proposal-advisor/document-revisi-proposal",
+    path: "/sistem-informasi-skripsi/daftar-bimbingan-proposal-advisor/document-revisi-proposal",
     element: <Page component={DocumentRevisiProposalAdvisor} />,
   },
+  // {
+  //   // Dokumen skripsi
+  //   path: "/sistem-informasi-skripsi/daftar-bimbingan-skripsi-advisor/dokumen-skripsi",
+  //   element: <Page component={} />,
+  // },
+  // {
+  //   // Berita Acara skripsi
+  //   path: "/sistem-informasi-skripsi/daftar-bimbingan-skripsi-advisor/berita-acara-skripsi",
+  //   element: <Page component={} />,
+  // },
+  // {
+  //   // Dokumen revisi skripsi
+  //   path: "/sistem-informasi-skripsi/daftar-bimbingan-skripsi-advisor/document-revisi-skripsi",
+  //   element: <Page component={} />,
+  // },
 ];
 
 export default advisorRoutes;
