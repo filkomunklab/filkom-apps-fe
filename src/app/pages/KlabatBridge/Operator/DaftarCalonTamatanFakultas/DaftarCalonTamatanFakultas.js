6ebc700d77d8aa900c2cd171d0ed940aacc7f420
tiffanyrambi <s2200180@student.unklab.ac.id>
2023-11-16 16:40:15 +0800

    DaftarCalonTamatan fakultas & register -> add search bar

--- src/app/pages/KlabatBridge/Operator/DaftarCalonTamatanFakultas/DaftarCalonTamatanFakultas.js
+++ src/app/pages/KlabatBridge/Operator/DaftarCalonTamatanFakultas/DaftarCalonTamatanFakultas.js
@@ -29,6 +29,7 @@ import {
   Checkbox,
   ListSubheader,
   TablePagination,
+  IconButton,
 } from "@mui/material";
 import ActionButton from "app/shared/ActionButton";
 import SearchGlobal from "app/shared/SearchGlobal";
@@ -38,6 +39,7 @@ import MarkunreadIcon from "@mui/icons-material/Markunread";
 import axios from "axios";
 import jwtAuthAxios from "app/services/Auth/jwtAuth";
 import {Document } from 'react-pdf'
+import SearchIcon from "@mui/icons-material/Search";
 
 const style = {
   position: "absolute",
@@ -437,8 +439,10 @@ const DaftarCalonTamatan = () => {
     </TableRow>
   );
 
-  const getData = async () => {
-    await jwtAuthAxios.get(`/spt?search_query=${searchValue}`).then((res) => {
+  React.useEffect(() => {
+    jwtAuthAxios
+      .get(`/spt?search_query=${searchValue}`)
+      .then((res) => {
       // await axios.get("http://localhost:2000/api/v1/spt/").then((res) => {
       console.log(res.data.data);
       const formattedData = res.data.data.map((item) => {
@@ -467,6 +471,10 @@ const DaftarCalonTamatan = () => {
 
       // console.log(uniqueGraduatePlan);
     });
+  }, [searchBtn]);
+
+  const getData = async () => {
+    
   };
 
   function filterData() {
@@ -478,18 +486,6 @@ const DaftarCalonTamatan = () => {
         item["graduate_plan"] === filterValue
     );
   }
-
-  // const dummy = [
-  //   {
-  //     name: 'yuhu',
-  //     approvalStatus: 'WAITING',
-  //     otherStatus: 'ACCEPTED',
-  //   }
-  // ]
-
-  React.useEffect(() => {
-    getData();
-  }, []);
 
   console.log(selectedData);
   return (
@@ -507,41 +503,67 @@ const DaftarCalonTamatan = () => {
         <Typography sx={{ fontSize: "24px", fontWeight: 500 }}>
           Graduate Candidates
         </Typography>
-        <FormControl sx={{ minWidth: 200 }} size="small">
-          <InputLabel htmlFor="grouped-select">Filter</InputLabel>
-          <Select
-            defaultValue=""
-            id="grouped-select"
-            label="Filter"
-            sx={{ borderRadius: 10, maxHeight: "50px" }}
-            value={filterValue}
-            onChange={(event) => setFilterValue(event.target.value)}
-          >
-            <MenuItem value="">
-              <em>None</em>
-            </MenuItem>
-            <ListSubheader sx={{ color: "#192739F0" }}>
-              Status by Faculty
-            </ListSubheader>
-            {statusByFac.map((item) => {
-              return <MenuItem value={item + "FACULTY"}>{item}</MenuItem>;
-            })}
+        <Div
+          sx={{
+            display: "flex",
+            direction: "row",
+            gap: 3,
+            alignItems: "center",
+          }}
+        >
+          {/* searchbar */}
+          <TextField
+            // label="Search"
+            placeholder="Search by Name or NIM"
+            variant="outlined"
+            size="small"
+            // value={searchTerm}
+            onChange={(e) => setSearchValue(e.target.value)}
+            InputProps={{
+              endAdornment: (
+                <IconButton onClick={() => setSearchBtn(!searchBtn)} edge="end">
+                  <SearchIcon />
+                </IconButton>
+              ),
+              style: { borderRadius: "25px", width: "250px", height: "35px" }, // Apply border radius here
+            }}
+          />
+          <FormControl sx={{ minWidth: 200 }} size="small">
+            <InputLabel htmlFor="grouped-select">Filter</InputLabel>
+            <Select
+              defaultValue=""
+              id="grouped-select"
+              label="Filter"
+              sx={{ borderRadius: 10, maxHeight: "50px" }}
+              value={filterValue}
+              onChange={(event) => setFilterValue(event.target.value)}
+            >
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              <ListSubheader sx={{ color: "#192739F0" }}>
+                Status by Faculty
+              </ListSubheader>
+              {statusByFac.map((item) => {
+                return <MenuItem value={item + "FACULTY"}>{item}</MenuItem>;
+              })}
 
-            <ListSubheader sx={{ color: "#192739F0" }}>
-              Status by Register
-            </ListSubheader>
-            {statusByRegister.map((item) => {
-              return <MenuItem value={item + "REGISTER"}>{item}</MenuItem>;
-            })}
+              <ListSubheader sx={{ color: "#192739F0" }}>
+                Status by Register
+              </ListSubheader>
+              {statusByRegister.map((item) => {
+                return <MenuItem value={item + "REGISTER"}>{item}</MenuItem>;
+              })}
 
-            <ListSubheader sx={{ color: "#192739F0" }}>
-              Rencana Tamat
-            </ListSubheader>
-            {graduatePlan.map((item) => {
-              return <MenuItem value={item}>{item}</MenuItem>;
-            })}
-          </Select>
-        </FormControl>
+              <ListSubheader sx={{ color: "#192739F0" }}>
+                Rencana Tamat
+              </ListSubheader>
+              {graduatePlan.map((item) => {
+                return <MenuItem value={item}>{item}</MenuItem>;
+              })}
+            </Select>
+          </FormControl>
+        </Div>
       </Div>
 
       <TableContainer component={Paper} sx={{ overflow: "auto" }}>
