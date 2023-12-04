03b8f39774a12f0d6f6e849a98e767eb3a0c8f4b
Hendra Luntungan <hendraluntungan29@gmail.com>
2023-12-04 20:06:26 +0800

    fix: slicing BA -> mahasiswa -> modify grades pages

--- src/app/pages/BimbinganAkademik/Mahasiswa/Grades/Grades.js
+++ src/app/pages/BimbinganAkademik/Mahasiswa/Grades/Grades.js
@@ -1,47 +1,91 @@
 import React from "react";
-import { Typography, Grid, experimentalStyled as styled, Paper } from "@mui/material";
+import {
+  Typography,
+  Grid,
+  experimentalStyled as styled,
+  Paper,
+  Stack,
+} from "@mui/material";
 import { Link } from "react-router-dom";
 
 const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "rgba(26, 56, 96, 0.1)",
+  backgroundColor:
+    theme.palette.mode === "dark" ? "#1A2027" : "rgba(26, 56, 96, 0.1)",
   ...theme.typography.body2,
   padding: theme.spacing(2),
   textAlign: "center",
   color: theme.palette.text.secondary,
-  cursor: "pointer", 
+  cursor: "pointer",
   transition: "background-color 0.3s ease-in-out",
 
   "&:hover": {
-    backgroundColor: theme.palette.mode === "dark" ? "rgba(255, 255, 255, 0.1)" : "rgba(0, 106, 254, 0.1)",
-    color: theme.palette.mode === "dark" ? "rgba(0, 95, 219, 1)" : "rgba(0, 95, 219, 1)",
+    backgroundColor:
+      theme.palette.mode === "dark"
+        ? "rgba(255, 255, 255, 0.1)"
+        : "rgba(0, 106, 254, 0.1)",
+    color:
+      theme.palette.mode === "dark"
+        ? "rgba(0, 95, 219, 1)"
+        : "rgba(0, 95, 219, 1)",
   },
-
 }));
 
 const Grades = () => {
-  const semesterNames = ["Semester 1", "Semester 2", "Semester 3", "Semester 4", "Semester 5"];
+  const semesterNames = [
+    { label: "Semester 1", value: 1 },
+    { label: "Semester 2", value: 2 },
+    { label: "Semester 3", value: 3 },
+    { label: "Semester 4", value: 4 },
+    { label: "Semester 5", value: 5 },
+  ];
+  const id = "105022010000";
 
   return (
     <div>
-      <Typography sx={{ fontSize: "24px", fontWeight: 500 }}>Student Grades</Typography>
-      <Typography sx={{ paddingTop: "22px", paddingBottom: "28px", fontSize: "15px", fontWeight: 400, color: "rgba(27, 43, 65, 0.69)" }}>Select a semester to view grades.</Typography>
-      <div sx={{ flexGrow: 1 }}>
-        <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-          {semesterNames.map((semester, index) => (
+      <Stack gap={3} paddingTop={3}>
+        <Stack direction={"row"} justifyContent={"space-between"}>
+          <Typography variant="h1" fontWeight={500}>
+            Student Grade
+          </Typography>
+        </Stack>
+        <Typography variant="h5">Select a semester to view grades.</Typography>
+        <Grid
+          container
+          spacing={{ xs: 2, md: 3 }}
+          columns={{ xs: 4, sm: 8, md: 12 }}
+        >
+          {semesterNames.reverse().map((semester, index) => (
             <Grid item xs={2} sm={4} md={4} key={index}>
-              <Link to="/bimbingan-akademik/grades/studentgrade/" style={{ textDecoration: "none" }}>
+              <Link
+                to={`/bimbingan-akademik/student-grade/${id}/grade/semester/${semester.value}`}
+                style={{ textDecoration: "none" }}
+              >
                 <Item>
-                  <Typography  sx={{ fontSize: "11px", fontWeight: "400", color: "rgba(27, 43, 65, 0.69)", textAlign: "left"  }}>
+                  <Typography
+                    sx={{
+                      fontSize: "11px",
+                      fontWeight: "400",
+                      color: "rgba(27, 43, 65, 0.69)",
+                      textAlign: "left",
+                    }}
+                  >
                     Student Grades
                   </Typography>
-                  <Typography sx={{ fontSize: "17px", fontWeight: "600", textAlign: "left"  }}>{semester} </Typography>
-                                   
+                  <Typography
+                    sx={{
+                      fontSize: "17px",
+                      fontWeight: "600",
+                      textAlign: "left",
+                    }}
+                  >
+                    {semester.label}
+                  </Typography>
                 </Item>
               </Link>
             </Grid>
           ))}
         </Grid>
-      </div>
+      </Stack>
     </div>
   );
 };
