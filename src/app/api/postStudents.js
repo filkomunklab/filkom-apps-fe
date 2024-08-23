d162f121b2f6e849046f7a10017a3949e2258bad
Darell Christopher Mona <95410660+DarellYuhu@users.noreply.github.com>
2024-08-23 08:21:37 +0000

    fix: api call with upload file feature fix(env)

--- src/app/api/postStudents.js
+++ src/app/api/postStudents.js
@@ -2,7 +2,7 @@ import { obeClient } from "./client";
 
 export default function postStudents(data) {
   const formData = new FormData();
-  formData.append("classMember", data.file);
+  formData.append("file", data.file);
   return obeClient.post(`/rps/${data.rpsId}/member`, formData, {
     headers: {
       "Content-Type": "multipart/form-data",
--- src/app/api/putStudentGrade.js
+++ src/app/api/putStudentGrade.js
@@ -3,7 +3,7 @@ import { obeClient } from "./client";
 export default function putStudentGrade(data) {
   const { file, gradingSystemId } = data;
   const formData = new FormData();
-  formData.append("grade", file);
+  formData.append("file", file);
   return obeClient.put(`/student-grade/${gradingSystemId}`, formData, {
     headers: { "Content-Type": "multipart/form-data" },
   });
